f8328ad01116ee8da5c8b6398c0f5eae3ae79fdb
Mateusz <kolbermateusz@gmail.com>
2025-04-06 19:26:28 +0200

    buttons

--- macOS.py
+++ macOS.py
@@ -1,6 +1,6 @@
 import os
 import getpass
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget,QPushButton, QHBoxLayout, QVBoxLayout, QRadioButton, QToolBar, QButtonGroup, QCheckBox
 def mac():
     print("mac")
     current_user = getpass.getuser()
@@ -10,9 +10,26 @@ def mac():
 class HomeMacOS(QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
     
     def initUI(self):
-        pass
+        self.button = QPushButton("Clean!")
+        self.radioButton1 = QCheckBox("Images")
+        self.radioButton2 = QCheckBox("Apps")
+        self.radioButton3 = QCheckBox("PDF")
+
+        self.buttonsBar = QToolBar()
+        self.master = QHBoxLayout()
+        myScreen = QVBoxLayout()
+
+        self.buttonsBar.addWidget(self.radioButton1)
+        self.buttonsBar.addWidget(self.radioButton2)
+        self.buttonsBar.addWidget(self.radioButton3)
+
+        myScreen.addWidget(self.buttonsBar)
+        myScreen.addWidget(self.button)
+        self.master.addLayout(myScreen,100)
+        self.setLayout(self.master)
 
     def settings(self):
         pass
